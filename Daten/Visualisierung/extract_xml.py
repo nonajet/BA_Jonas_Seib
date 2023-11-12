c68b5c77eac25db5775cb3547bfef2dd052be8f1
nonajet <jonas.seib@gmail.com>
2023-11-12 12:04:23 +0100

    added global vis. on mat and total view of all steps

--- Daten/Visualisierung/extract_xml.py
+++ Daten/Visualisierung/extract_xml.py
@@ -3,34 +3,49 @@ import xml.etree.ElementTree as et
 import numpy as np
 import matplotlib.pyplot as plt
 
+NS = {'zb': 'http://www.zebris.de/measurements'}
 
-def get_cell_count(filepath):  # not robustified yet
+
+def get_cell_count(filepath, id):  # not robustified yet
+    movement = get_movement(filepath, id)
+    cc_tuple = movement.find('.//zb:cell_count', NS).text
+    x = int(cc_tuple[0])
+    y = int(cc_tuple[1])
+    return y, x
+
+
+def get_count(filepath, id):  # get No. of matrices in one data set
+    movement = get_movement(filepath, id)
+    return int(movement.find('.//zb:count', NS).text)
+
+
+def get_movement(filepath, id):
     root = et.parse(filepath).getroot()
-    x = int(root[8][0].text)  # 9th child from root is cell_count
-    y = int(root[8][1].text)
-    return x, y
+    movements = root.findall('.//zb:movement', NS)
+    for movement in movements:
+        if movement.find('.//zb:id', NS).text == id:
+            return movement
 
 
-def get_count(filepath):  # get No. of matrices in one data set
-    root = et.parse(filepath).getroot()
-    for attr in root.iter('count'):
-        count = int(attr.text)
-        print(count)
-
-
-def extract_matrices(filepath):
-    root = et.parse(filepath).getroot()
+def extract_matrices(filepath, id):
+    root = get_movement(filepath, id)
 
     # extract matrix data from xml
-    quant_elements = root.findall(".//data/quant")
+    quant_elements = root.findall(".//zb:data/zb:quant", NS)
     matrices = []  # temp. array for matrices from xml; may contain empty lists too
+    cell_begin = []
     for quant_element in quant_elements:
         quant_data = {
             "cells": [
-                list(map(float, cell_data.split())) for cell_data in quant_element.find("cells").text.splitlines()
+                list(map(float, cell_data.split())) for cell_data in
+                quant_element.find("zb:cells", NS).text.splitlines()
             ],
         }
         matrices.append(quant_data)
+
+        tmp_x = int(quant_element.find('.//zb:cell_begin/zb:x', NS).text)
+        tmp_y = int(quant_element.find('.//zb:cell_begin/zb:y', NS).text)
+        cell_begin.append([tmp_x, tmp_y])
 
     data = []  # final list with matrices
 
@@ -41,42 +56,68 @@ def extract_matrices(filepath):
                 tmp_matrix.append(rows)
         data.append(tmp_matrix)
 
-    # print(data[0])
-    # print('----------------------------------------new data set----------------------------------------')
-    # np_matrix = np.array(data, dtype=object)
-
-    return data
+    return data, cell_begin
 
 
-def global_max(matrix):  # not tested or used yet; used for finding global max in one or more matrices
-    gmax = -1
-    for mx in matrix:
-        for row in mx:
-            if max(row) > global_max:
-                gmax = max(row)
-    return gmax
-
-
-def visualize(matrix):
-    fig, ax = plt.subplots()
+def visualize(filepath, id):
+    matrix, offset = extract_matrices(filepath, id)
+    fig, ax = plt.subplots(1, 3)
     plt.ion()
 
+    ax_local = ax[0]
+
+    ax_global = ax[1]
+
+    ax_total = ax[2]
+    total_mx = np.zeros((481, 64))
+
+    mx_ctr = 0
     for mx in matrix:
-        # if mx == []
-        # fig.colorbar(im, ax=ax, label='Interactive colorbar')
+        if mx and mx_ctr % 5 == 0:
+            mx_np = np.array(mx)
 
-        mx_np = np.matrix(mx)
-        ax.matshow(mx_np)
-        plt.pause(0.01)
-        ax.clear()
+            # local
+            ax_local.matshow(np.flipud(np.fliplr(mx_np)))
 
-        # caxes = axes.matshow(mx, interpolation='nearest')
-        # figure.colorbar(caxes)
+            # global
+            ax_global.set_xlim([0, 63])
+            ax_global.set_ylim([0, 480])
+            global_mx = create_global_mx(mx_np, offset[mx_ctr])
+            ax_global.matshow(global_mx)
+
+            # total
+            ax_total.set_xlim([0, 63])
+            ax_total.set_ylim([0, 480])
+            total_mx += global_mx
+            ax_total.matshow(total_mx)
+
+            plt.pause(0.0001)
+            ax_global.clear()
+            ax_local.clear()
+            ax_total.clear()
+        mx_ctr += 1
+
+    print(
+        '############################################\nmovement (%s) '
+        'finished\n############################################' % id)
+
+
+def create_global_mx(local_mx, offset):
+    global_mx = np.zeros((481, 64))
+    x_off = offset[1] - 1
+    if x_off < 0: x_off = 0
+    y_off = offset[0] - 1
+    if y_off < 0: y_off = 0
+    rows = local_mx.shape[0]
+    cols = local_mx.shape[1]
+
+    try:
+        global_mx[-1 - x_off - rows:-1 - x_off, y_off:y_off + cols] = local_mx
+    except ValueError as e:
+        print(e)
+    return global_mx
 
 
 if __name__ == '__main__':
-    filep = r'C:\Users\jonas\OneDrive\Desktop\Studium_OvGU\WiSe23_24\BA\Daten\Rohdaten\test2.xml'
-    a = extract_matrices(r'C:\Users\jonas\OneDrive\Desktop\Studium_OvGU\WiSe23_24\BA\Daten\Rohdaten\test2.xml')
-    # print(a)
-    visualize(a)
-    # print(get_cell_count(filep))
+    filep = r'C:\Users\jonas\OneDrive\Desktop\Studium_OvGU\WiSe23_24\BA\Daten\Rohdaten\T0307068 Trab.xml'
+    visualize(filep, 'gait_3')
